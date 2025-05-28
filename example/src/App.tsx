ef7e8a99cd4f5c2efa765a67dc8e9f1577e597db
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2025-05-28 10:02:38 +0200

    feat: initial measure

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,26 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { useRef } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
 import { multiply } from 'react-native-shadow-nodes-experiments';
+
+import ReactNativeInterface from 'react-native/Libraries/ReactPrivate/ReactNativePrivateInterface';
 
 const result = multiply(3, 7);
 
 export default function App() {
+  const textRef = useRef(null);
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Text ref={textRef}>Result: {result}</Text>
+      <Button
+        title="Click me"
+        onPress={() => {
+          const shadowNode = ReactNativeInterface.getNodeFromPublicInstance(
+            textRef.current
+          );
+          // @ts-ignore
+          globalThis.__testFunc(shadowNode);
+        }}
+      />
     </View>
   );
 }
--- ios/ShadowNodesExperiments.mm
+++ ios/ShadowNodesExperiments.mm
@@ -1,4 +1,16 @@
 #import "ShadowNodesExperiments.h"
+#import <jsi/jsi.h>
+#import "RCTTurboModuleWithJSIBindings.h"
+#include <iostream>
+#include <react/renderer/uimanager/primitives.h>
+#include <react/renderer/core/LayoutableShadowNode.h>
+
+using namespace facebook;
+using namespace facebook::react;
+
+@interface ShadowNodesExperiments () <RCTTurboModuleWithJSIBindings>
+
+@end
 
 @implementation ShadowNodesExperiments
 RCT_EXPORT_MODULE()
@@ -7,6 +19,30 @@ RCT_EXPORT_MODULE()
     NSNumber *result = @(a * b);
 
     return result;
+}
+
+- (void)installJSIBindingsWithRuntime:(facebook::jsi::Runtime &)runtime callInvoker:(const std::shared_ptr<facebook::react::CallInvoker> &)callinvoker {
+  
+  auto testFunc = jsi::Function::createFromHostFunction(
+                    runtime,
+                    jsi::PropNameID::forAscii(runtime, "__testFunc"),
+                    1,
+                    [](
+                      jsi::Runtime& runtime,
+                      const jsi::Value& /*thisValue*/,
+                      const jsi::Value* arguments,
+                      size_t count) -> jsi::Value {
+                        ShadowNode::Shared shadowNode = shadowNodeFromValue(runtime, arguments[0]);
+                        auto layoutableShadowNode =
+                             dynamic_pointer_cast<const LayoutableShadowNode>(shadowNode);
+                        
+                        auto layout = layoutableShadowNode->getLayoutMetrics();
+                        std::cout << "width: " << layout.frame.size.width << " height: " << layout.frame.size.height;
+                         
+                         return jsi::Value::undefined();
+                  });
+  
+  runtime.global().setProperty(runtime, "__testFunc", std::move(testFunc));
 }
 
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
